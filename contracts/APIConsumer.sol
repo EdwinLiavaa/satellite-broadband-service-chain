fd9c4fda2e21a6d123e5dce4ee6fa84a08508c85
FidelChe <etuini.liavaa@gmail.com>
2021-11-14 12:28:20 +1100

    Chainlink Hackathon Fall 2021

--- contracts/APIConsumer.sol
+++ contracts/APIConsumer.sol
@@ -9,28 +9,22 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 contract APIConsumer is ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
-    uint256 public volume;
+    uint256 public location;
 
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
-    /**
-     * Network: Kovan
-     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel
-     * Node)
-     * Job ID: d5270d1c311941d0b08bead21fea7747
-     * Fee: 0.1 LINK
-     */
+
     constructor(address _oracle, string memory _jobId, uint256 _fee, address _link) public {
         if (_link == address(0)) {
             setPublicChainlinkToken();
         } else {
             setChainlinkToken(_link);
         }
-        // oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
-        // jobId = "29fa9aa13bf1468788b7cc4a500a45b8";
-        // fee = 0.1 * 10 ** 18; // 0.1 LINK
+        oracle = 0xB37424e282D7EE4AbDF43B7cC9f81D5f81539ccf;
+        jobId = "85d1c04c4bad429ab247fe503d0175cd";
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
         oracle = _oracle;
         jobId = stringToBytes32(_jobId);
         fee = _fee;
@@ -40,12 +34,12 @@ contract APIConsumer is ChainlinkClient {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestVolumeData() public returns (bytes32 requestId)
+    function requestMarineData() public returns (bytes32 requestId)
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        request.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
+        //request.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
 
         // Set the path to find the desired data in the API response, where the response format is:
         // {"RAW":
@@ -57,11 +51,7 @@ contract APIConsumer is ChainlinkClient {
         //    }
         //   }
         //  }
-        request.add("path", "RAW.ETH.USD.VOLUME24HOUR");
-
-        // Multiply the result by 1000000000000000000 to remove decimals
-        int timesAmount = 10**18;
-        request.addInt("times", timesAmount);
+        request.add("shipId", "9525338");
 
         // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
@@ -70,9 +60,9 @@ contract APIConsumer is ChainlinkClient {
     /**
      * Receive the response in the form of uint256
      */
-    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId)
+    function fulfill(bytes32 _requestId, uint256 _location) public recordChainlinkFulfillment(_requestId)
     {
-        volume = _volume;
+        location = _location;
     }
 
     function stringToBytes32(string memory source) public pure returns (bytes32 result) {
